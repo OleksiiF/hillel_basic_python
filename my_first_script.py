3cf744c7f0721fadaa3aa469a0d2848c394219b6
Oleksii <you@example.com>
2022-10-20 21:36:30 +0300

    data types

--- my_first_script.py
+++ my_first_script.py
@@ -1,1 +1,70 @@
-print("Hello World")
+# print("Hello World")
+#
+# my_int_var = 10
+# print(type(my_int_var))
+#
+# my_str_var = 'dfkjghjkfd'
+# print(type(my_str_var))
+#
+# # sum of two numbers
+# result = 1 + 1  # sum of two numbers
+#
+#
+# some_variable = 13
+# # some_variable -> ім'я об'єкта
+# # 13 -> об'єкт
+# # = -> присвоєння імені об'єкту
+#
+#
+# some_variable = 13
+# # some_variable -> змінна
+# # 13 -> значення змінної
+# # = -> присвоєння значення змінної
+
+#############################################################################
+# number_a = 7 + 1      # straight method
+# number_b = number_a   # reference to another object
+# print(number_a, number_b)
+#
+# number_a, number_b = 4, 12  # multiple assignment exist, not recommended
+# print(number_a, number_b)
+#
+# number_a, number_b = number_b, number_a  # magic
+# print(number_a, number_b)
+#############################################################################
+# print(type(-3))
+# print(type("some string"))
+#
+# some_value = 7
+# print(type(some_value))
+#############################################################################
+# variable_one: int = 10
+# variable_one_float: float = 10.0
+# variable_two: str = "10"
+# variable_three: int = "string legitime here"
+# # Annotation does not set the type of var
+# # It's just a tip for the developer, who will read the code
+#############################################################################
+# my_int_value_a = 5
+# my_int_value_b = 5
+#
+# print(id(6))
+# print(id(5))
+# print(id(my_int_value_a))
+# print(id(my_int_value_b))
+# my_int_value_b = 6
+# print(id(my_int_value_b))
+#############################################################################
+# my_result = print(13)
+# print(my_result)
+# print('my result "ok"')
+# print("my_result o'k")
+#
+#
+# avg_age = 14
+# name_postman = "John"
+# print(avg_age, name_postman)
+# print('2', ', ', '4')
+# print('asdfsadf' + ', ' + 'kjsadhjfkjsdahfkljsadf')
+# print(f'my name is {name_postman}, {my_result}')
+# print(name_postman)
